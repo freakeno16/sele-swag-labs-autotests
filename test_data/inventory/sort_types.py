e75e8101123329c95c7b9514889c51a2a19fa212
Никита В. Откидач <Otkidachnv@ac.local>
2026-09-03 15:28:01 +0500

    fix: rename sort types values for correct calling by DOM values

--- test_data/inventory/sort_types.py
+++ test_data/inventory/sort_types.py
@@ -1,6 +1,6 @@
 SORT_TYPES = {
-    "name_a_to_z": "Name (A to Z)",
-    "name_z_to_a": "Name (Z to A)",
+    "name_a_to_z": "az",
+    "name_z_to_a": "za",
     "price_low_to_high": "lohi",
-    "price_high_to_low": "Price (high to low)"
+    "price_high_to_low": "hilo"
 }
