6d9b1be86417ae601de10813d2635ea8cc944831
Никита В. Откидач <Otkidachnv@ac.local>
2026-09-03 15:31:48 +0500

    refactor: uncomment tests and remove unused import

--- tests/inventory/test_sorting_items.py
+++ tests/inventory/test_sorting_items.py
@@ -1,20 +1,19 @@
-import time
 from test_data.inventory.sort_types import SORT_TYPES
 
 def test_sort_items_by_price_from_low_to_high(auth_inventory_page):
     auth_inventory_page.sort_items_by(SORT_TYPES["price_low_to_high"])
     auth_inventory_page.check_items_sorting_by(SORT_TYPES["price_low_to_high"])
 
-# def test_sort_items_by_price_from_high_to_low(auth_inventory_page):
-#     auth_inventory_page.sort_items_by(SORT_TYPES["price_high_to_low"])
-#     auth_inventory_page.check_items_sorting_by(SORT_TYPES["price_high_to_low"])
-#
-#
-# def test_sort_items_by_name_from_a_to_z(auth_inventory_page):
-#     auth_inventory_page.sort_items_by(SORT_TYPES["name_a_to_z"])
-#     auth_inventory_page.check_items_sorting_by(SORT_TYPES["name_a_to_z"])
-#
-#
-# def test_sort_items_by_name_from_z_to_a(auth_inventory_page):
-#     auth_inventory_page.sort_items_by(SORT_TYPES["name_z_to_a"])
-#     auth_inventory_page.check_items_sorting_by(SORT_TYPES["name_z_to_a"])
+def test_sort_items_by_price_from_high_to_low(auth_inventory_page):
+    auth_inventory_page.sort_items_by(SORT_TYPES["price_high_to_low"])
+    auth_inventory_page.check_items_sorting_by(SORT_TYPES["price_high_to_low"])
+
+
+def test_sort_items_by_name_from_a_to_z(auth_inventory_page):
+    auth_inventory_page.sort_items_by(SORT_TYPES["name_a_to_z"])
+    auth_inventory_page.check_items_sorting_by(SORT_TYPES["name_a_to_z"])
+
+
+def test_sort_items_by_name_from_z_to_a(auth_inventory_page):
+    auth_inventory_page.sort_items_by(SORT_TYPES["name_z_to_a"])
+    auth_inventory_page.check_items_sorting_by(SORT_TYPES["name_z_to_a"])
