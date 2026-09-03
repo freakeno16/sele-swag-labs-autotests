e080aba9de08429874da20cfbf15bd009dcb493d
Никита В. Откидач <Otkidachnv@ac.local>
2026-09-03 15:26:45 +0500

    add sort check method with all sort types, need to refactor

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -6,10 +6,11 @@ from test_data.inventory.sort_types import SORT_TYPES
 
 class InventoryPage(BasePage):
 
-    INVENTORY = (By.CLASS_NAME, "inventory_container")
-    SORT_SELECT_FIELD = (By.CLASS_NAME, "product_sort_container")
-    INVENTORY_ITEM = (By.CLASS_NAME, "inventory_item")
-    INVENTORY_PRICE = (By.CLASS_NAME, "inventory_item_price")
+    INVENTORY = (By.CSS_SELECTOR, "[data-test='inventory-list']")
+    SORT_SELECT_FIELD = (By.CSS_SELECTOR, "[data-test='product-sort-container']")
+    INVENTORY_ITEM = (By.CSS_SELECTOR, "[data-test='inventory-item']")
+    INVENTORY_ITEM_PRICE = (By.CSS_SELECTOR, "[data-test='inventory-item-price']")
+    INVENTORY_ITEM_NAME = (By.CSS_SELECTOR, "[data-test='inventory-item-name']")
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -19,13 +20,35 @@ class InventoryPage(BasePage):
         assert self.find(self.INVENTORY).is_displayed()
 
     def formate_prices(self):
-        inventory_prices = self.driver.find_elements(*self.INVENTORY_PRICE)
-        for price in inventory_prices:
-            print(price.text)
+        inventory_prices = self.driver.find_elements(*self.INVENTORY_ITEM_PRICE)
         formatted_prices = [float(price.text.replace('$', '')) for price in inventory_prices]
-        print(formatted_prices)
+
         return formatted_prices
 
     def sort_items_by(self, sort_type):
         select_field = Select(self.find(self.SORT_SELECT_FIELD))
         select_field.select_by_value(sort_type)
+
+    def check_items_sorting_by(self, sort_type):
+        if sort_type == SORT_TYPES["price_low_to_high"]:
+            formatted_prices = self.formate_prices()
+
+            assert sorted(formatted_prices) == formatted_prices
+
+        elif sort_type == SORT_TYPES["price_high_to_low"]:
+            formatted_prices = self.formate_prices()
+
+            assert sorted(formatted_prices, reverse=True) == formatted_prices
+
+        elif sort_type == SORT_TYPES["name_a_to_z"]:
+            inventory_names_elements = self.driver.find_elements(*self.INVENTORY_ITEM_NAME)
+            inventory_names = [name.text for name in inventory_names_elements]
+
+            assert inventory_names == sorted(inventory_names)
+
+        else:
+            inventory_names_elements = self.driver.find_elements(*self.INVENTORY_ITEM_NAME)
+            inventory_names = [name.text for name in inventory_names_elements]
+
+            assert inventory_names == sorted(inventory_names, reverse=True)
+
